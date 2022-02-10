a2acb126e9307c4d039884fce8495fddbd188ebf
Jbudd512 <Jbudd0512@gmail.com>
2022-02-10 15:27:11 -0600

    moved routing back into main file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import mockApartments from "./assets/mockApartments";
+import Home from "./pages/Home";
+import AptIndex from "./pages/AptIndex";
+import AptIndexProt from "./pages/AptIndexProt";
+import AptShow from "./pages/AptShow";
+import AptNew from "./pages/AptNew";
+import AptEdit from "./pages/AptEdit";
+import AptDelete from "./pages/AptDelete";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -22,6 +31,23 @@ class App extends Component {
       <>
         <Header apartments={this.state.apartments} {...this.props} />
         <Footer />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              path="/aptindex"
+              render={(props) => (
+                <AptIndex apartments={this.state.apartments} />
+              )}
+            />
+            <Route path="/aptindexprot" component={AptIndexProt} />
+            <Route path="/aptshow" component={AptShow} />
+            <Route path="/aptnew" component={AptNew} />
+            <Route path="/aptedit" component={AptEdit} />
+            <Route path="/aptdelete" component={AptDelete} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
       </>
     );
   }
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,13 +1,4 @@
 import React, { Component } from "react";
-import Home from "../pages/Home";
-import AptIndex from "../pages/AptIndex";
-import AptIndexProt from "../pages/AptIndexProt";
-import AptShow from "../pages/AptShow";
-import AptNew from "../pages/AptNew";
-import AptEdit from "../pages/AptEdit";
-import AptDelete from "../pages/AptDelete";
-import NotFound from "../pages/NotFound";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Nav, NavItem, NavLink } from "reactstrap";
 
 class Header extends Component {
@@ -22,41 +13,24 @@ class Header extends Component {
     return (
       <>
         <h1>Header</h1>
-        <Router>
-          <Nav>
-            <NavItem>
-              <ul>
-                <li>
-                  <NavLink href="/">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink href="/aptindex">See all apartments</NavLink>
-                </li>
-                <li>
-                  <NavLink href={sign_in_route}>Sign in</NavLink>
-                </li>
-                <li>
-                  <NavLink href={sign_out_route}>Sign out</NavLink>
-                </li>
-              </ul>
-            </NavItem>
-          </Nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route
-              path="/aptindex"
-              render={(props) => (
-                <AptIndex apartments={this.props.apartments} />
-              )}
-            />
-            <Route path="/aptindexprot" component={AptIndexProt} />
-            <Route path="/aptshow" component={AptShow} />
-            <Route path="/aptnew" component={AptNew} />
-            <Route path="/aptedit" component={AptEdit} />
-            <Route path="/aptdelete" component={AptDelete} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
+        <Nav>
+          <NavItem>
+            <ul>
+              <li>
+                <NavLink href="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink href="/aptindex">See all apartments</NavLink>
+              </li>
+              <li>
+                <NavLink href={sign_in_route}>Sign in</NavLink>
+              </li>
+              <li>
+                <NavLink href={sign_out_route}>Sign out</NavLink>
+              </li>
+            </ul>
+          </NavItem>
+        </Nav>
       </>
     );
   }
