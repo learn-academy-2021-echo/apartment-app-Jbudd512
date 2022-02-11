654741e19e7a13b842aa9fd385cb94a5b9eb4812
Jbudd512 <Jbudd0512@gmail.com>
2022-02-11 12:30:45 -0600

    user login/logout setup

--- app/javascript/components/pages/AptShow.js
+++ app/javascript/components/pages/AptShow.js
@@ -31,14 +31,3 @@ class AptShow extends Component {
 }
 
 export default AptShow;
-
-// street: "12345 Generic Street Name";
-// city: "A place";
-// state: "Somewhere";
-// manager: "A Shady Company";
-// email: "54321generic@gmail.com";
-// price: "Tree Fiddy";
-// bedrooms: 1;
-// bathrooms: 1;
-// pets: "Nothing exotic";
-// user_id: 1;
